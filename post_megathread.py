0c4be5d6edde4ff7321b64087f9d719df0631ce8
Wazo Heat <wazoheat@gmail.com>
2021-03-24 19:20:45 -0600

    - Fix noob python mistake, passing lists to a class - Set urls for OutlookType and WatchType classes - Define functions to populate watches and outlooks with more information from their respective pages   - populate_watches is nearly done, populate_risks is coming soon - Add logic to populate jinja template with watch information   - Need to clean up the formatting

--- post_megathread.py
+++ post_megathread.py
@@ -10,12 +10,13 @@ import datetime
 
 
 class WatchType:
-    def __init__(self, no=0, type="Severe Thunderstorm", pds=False, area="", threats=""):
+    def __init__(self, no=0, type="Severe Thunderstorm", pds=False, **kwargs):
         self.no = no
         self.type = type
         self.pds = pds
-        self.area = area
-        self.threats = threats
+        self.area = kwargs.get('area', [])
+        self.threats = kwargs.get('threats', [])
+        self.url = 'https://www.spc.noaa.gov/products/watch/ww' + self.no.zfill(4) + '.html' 
 
 class OutlookType:
     now=datetime.datetime.now()
@@ -25,6 +26,7 @@ class OutlookType:
         self.yyyymmdd = yyyymmdd
         self.time_utc = time_utc
         self.summary = summary_text
+        self.url = "https://www.spc.noaa.gov/products/outlook/day" + str(self.day) + "otlk.html"
         if self.risk == "Enhanced":
             self.arisk = "an " + self.risk
         else:
@@ -52,7 +54,8 @@ def check_risks(fn):
 
         return outlooks
 
-#        Forecast Risk of Severe Storms: <span class="enhanced">Enhanced Risk</span>
+def populate_risks(outlooks):
+    return outlooks
 
 def check_watches(fn):
     with open(fn) as fp:
@@ -79,6 +82,58 @@ def check_watches(fn):
 
         return watches
 
+def populate_watches(watches):
+    print(len(watches))
+    for watch in watches:
+
+        fn_watch="watch" + watch.no.zfill(4) + ".txt"
+        print("Opening ",fn_watch)
+        if args.gotime:
+            res = requests.get(watch.url)
+            if res.status_code != 200:
+                print('WARNING: potentially unsuccessful HTTP status code: ', res.status_code)
+            print(res.text, file=open(fn_watch, 'w'))
+        else:
+            print('Running in debug mode; specify argument "--gotime" to run the real deal')
+        with open(fn_watch) as fp:
+            while True:
+                line = fp.readline()
+
+                if not line:
+                    break
+
+                # We need to check that watch.area and watch.threats are empty,
+                # because the watch text appears twice on the page and we don't want to repeat ourselves
+                if "Watch for portions of" in line and not watch.area:
+                    line=fp.readline()
+                    while line.strip(): 
+                        # Add a semicolon unless the line is indented, indicating it continues previous area line
+                        if re.search(r'^      ', line ):
+                            watch.area[-1] = watch.area[-1] + line.strip()
+                        else:
+                            watch.area.append(line.strip())
+                        line=fp.readline()
+
+                if "* Primary threats include..." in line and not watch.threats:
+                    line=fp.readline()
+                    while line.strip():
+                        # If line is indented, this indicates it continues previous threat line
+                        if re.search(r'^      ', line ):
+                            watch.threats[-1] = watch.threats[-1] + line.strip()
+                        else:
+                            watch.threats.append(line.strip())
+                        line=fp.readline()
+
+        if watch.pds:
+            print("* [**Particularly Dangerous Situation (PDS)** " + watch.type + " Watch " + watch.no + "](" + watch.url + "): portions of")
+        else:
+            print("* [" + watch.type + " Watch " + watch.no + "](" + watch.url + "): portions of")
+        print(watch.area)
+        print("\nPrimary threats include...")
+        print(watch.threats)
+
+    return watches
+
 def post(subr,title,template_file,outlook,watches,post):
     credentials = 'client_secrets.json'
     with open(credentials) as f:
@@ -100,7 +155,22 @@ def post(subr,title,template_file,outlook,watches,post):
     templateEnv = j2.Environment(loader=templateLoader)
     template = templateEnv.get_template(template_file)
 
-    selftext = template.render(risk_level=outlook.risk,arisk=outlook.arisk,num_watches=len(watches),day_of_week=now.strftime("%A"),month=now.strftime("%B"),dd=outlook.yyyymmdd[-2:],yyyy=outlook.yyyymmdd[:4])
+    #Define summary text and watches text
+    watches_text=[]
+    for watch in watches:
+        if watch.pds:
+            watches_text.append("* [**Particularly Dangerous Situation (PDS)** " + watch.type + " Watch " + watch.no + "](" + watch.url + "): portions of")
+        else:
+            watches_text.append("* [" + watch.type + " Watch " + watch.no + "](" + watch.url + "): portions of")
+        watches_text.append(watch.area)
+        watches_text.append("\nPrimary threats include...")
+        watches_text.append(watch.threats)
+
+    if not watches_text:
+        watches_text="* *None in effect*"
+
+
+    selftext = template.render(risk_level=outlook.risk,arisk=outlook.arisk,num_watches=len(watches),day_of_week=now.strftime("%A"),month=now.strftime("%B"),dd=outlook.yyyymmdd[-2:],yyyy=outlook.yyyymmdd[:4],watches_text=watches_text)
 
     if not title:
         title="Day " + str(outlook.day) + " severe weather outlook for " + now.strftime("%A") + ", " + now.strftime("%B") + " " + outlook.yyyymmdd[-2:] +", " + outlook.yyyymmdd[:4]
@@ -127,13 +197,13 @@ if __name__ == '__main__':
         fn_outlooks="outlooks.txt"
         fn_watches="watches.txt"
         res = requests.get('https://www.spc.noaa.gov/products/outlook/')
+        if res.status_code != 200:
+            print('WARNING: potentially unsuccessful HTTP status code: ', res.status_code)
         print(res.text, file=open(fn_outlooks, 'w'))
-        if res.status_code != 200:
-            print('WARNING: potentially unsuccessful HTTP status code: ', res.status_code)
         res = requests.get('https://www.spc.noaa.gov/products/watch/')
-        print(res.text, file=open(fn_watches, 'w'))
         if res.status_code != 200:
             print('WARNING: potentially unsuccessful HTTP status code: ', res.status_code)
+        print(res.text, file=open(fn_watches, 'w'))
 
     else:
         print('Running in debug mode; specify argument "--gotime" to run the real deal')
@@ -143,24 +213,34 @@ if __name__ == '__main__':
 #Check general severe risk for Day 1
     outlooks=check_risks(fn_outlooks)
 
-#Check for active watches
+#Check for active watches, grab watch info, populate watch objects
     watches=check_watches(fn_watches)
 
-    if not watches:
-        print("No watches in effect")
-    else:
+    if watches:
+        watches=populate_watches(watches)
         print("Watches in effect:")
         for watch in watches:
             print(watch.type + " Watch " + watch.no)
             if watch.pds:
                 print("PARTICULARLY DANGEROUS SITUATION")
             print("")
+    else:
+        print("No watches in effect")
 
     submissions=[]
     for outlook in outlooks:
         print("Day " + str(outlook.day) + " outlook: " + outlook.risk)
         risk_post_levels = {'Enhanced', 'Moderate', 'High'}
         if outlook.risk in risk_post_levels:
+            fn_outlook="day" + str(outlook.day) + "outlook.txt"
+            if args.gotime:
+                res = requests.get(outlook.url)
+                if res.status_code != 200:
+                    print('WARNING: potentially unsuccessful HTTP status code: ', res.status_code)
+                print(res.text, file=open(fn_outlook, 'w'))
+                if res.status_code != 200:
+                    print('WARNING: potentially unsuccessful HTTP status code: ', res.status_code)
+
             submissions.append(post("wazoheat","","jinja_template.md",outlook,watches,args.post))
 
     if submissions:
